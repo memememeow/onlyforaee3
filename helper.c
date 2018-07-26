0bced04ea2b71b721484d891d56bbd2188f2d47e
Yuhan Shao <yuhan.shao@mail.utoronto.ca>
2018-07-26 01:41:00 -0400

    Helper functions

--- helper.c
+++ helper.c
@@ -1,1 +1,75 @@
-// Implement helper.c
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "ext2.h"
+
+#define SINGLE_INDIRECT 12
+
+/*
+ * Return the disk location.
+ */
+unsigned char *get_disk_loc(char *disk_name) {
+    int fd = open(disk_name, O_RDWR);
+
+    // Map disk image file into memory
+    unsigned char *disk = mmap(NULL, 128 * 1024, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if(disk == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+
+    return disk;
+}
+
+/*
+ * Return the super block location.
+ */
+struct ext2_super_block *get_superblock_loc(unsigned char *disk) {
+    return (struct ext2_super_block *)(disk + 1024);
+}
+
+/*
+ * Return the group descriptor location.
+ */
+struct ext2_group_desc *get_group_descriptor_loc(unsigned char *disk) {
+    return (struct ext2_group_desc *)(disk + 2 * EXT2_BLOCK_SIZE);
+}
+
+/*
+ * Return the block bitmap (16*8 bits) location.
+ */
+unsigned char *get_block_bitmap_loc(unsigned char *disk, struct ext2_group_desc *gd) {
+    return disk + EXT2_BLOCK_SIZE * (gd->bg_block_bitmap);
+}
+
+/*
+ * Return the inode bitmap (4*8 bits) location.
+ */
+unsigned char *get_inode_bitmap_loc(unsigned char *disk, struct ext2_group_desc *gd) {
+    return disk + EXT2_BLOCK_SIZE * (gd->bg_inode_bitmap);
+}
+
+/*
+ * Return the inode table location.
+ */
+struct ext2_inode  *get_inode_table_loc(unsigned char *disk, struct ext2_group_desc *gd) {
+    return (struct ext2_inode *) (disk + EXT2_BLOCK_SIZE * gd->bg_inode_table);
+}
+
+/*
+ * Return the indirect block location.
+ */
+unsigned int *get_indirect_block_loc(unsigned char *disk, struct ext2_inode  *inode, int node) {
+    return (unsigned int *) (disk + EXT2_BLOCK_SIZE * inode[node].i_block[SINGLE_INDIRECT]);
+}
+
+/*
+ * Return the directory location.
+ */
+struct ext2_dir_entry_2 *get_directory_loc(unsigned char *disk, struct ext2_inode  *inode, int node, int i_block) {
+    return (struct ext2_dir_entry_2 *) (disk + EXT2_BLOCK_SIZE * (inode[node].i_block[i_block]));
+}
