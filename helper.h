686d1ee02a2e7d0c857aa5dd1d6040cec6af967f
Yuhan Shao <yuhan.shao@mail.utoronto.ca>
2018-07-26 01:50:35 -0400

    helper.h

--- helper.h
+++ helper.h
@@ -1,1 +1,13 @@
-//Add declarations of helper functions and macros
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ext2.h"
+
+unsigned char *get_disk_loc(char *disk_name);
+struct ext2_super_block *get_superblock_loc(unsigned char *disk);
+struct ext2_group_desc *get_group_descriptor_loc(unsigned char *disk);
+unsigned char *get_block_bitmap_loc(unsigned char *disk, struct ext2_group_desc *gd);
+unsigned char *get_inode_bitmap_loc(unsigned char *disk, struct ext2_group_desc *gd);
+struct ext2_inode  *get_inode_table_loc(unsigned char *disk, struct ext2_group_desc *gd);
+unsigned int *get_indirect_block_loc(unsigned char *disk, struct ext2_inode  *inode, int node);
+struct ext2_dir_entry_2 *get_directory_loc(unsigned char *disk, struct ext2_inode  *inode, int node, int i_block);
