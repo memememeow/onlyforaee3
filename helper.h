d5d00a7c3fcc327caffd59069da329c33a156bfe
Yuhan Shao <yuhan.shao@mail.utoronto.ca>
2018-07-26 12:50:45 -0400

    helper

--- helper.h
+++ helper.h
@@ -8,7 +8,7 @@ struct ext2_super_block *get_superblock_loc(unsigned char *disk);
 struct ext2_group_desc *get_group_descriptor_loc(unsigned char *disk);
 unsigned char *get_block_bitmap_loc(unsigned char *disk, struct ext2_group_desc *gd);
 unsigned char *get_inode_bitmap_loc(unsigned char *disk, struct ext2_group_desc *gd);
-struct ext2_inode  *get_inode_table_loc(unsigned char *disk, struct ext2_group_desc *gd);
+struct ext2_inode *get_inode_table_loc(unsigned char *disk, struct ext2_group_desc *gd);
 unsigned int *get_indirect_block_loc(unsigned char *disk, struct ext2_inode  *inode);
 struct ext2_dir_entry_2 *get_directory_loc(unsigned char *disk, struct ext2_inode  *inode, int i_block);
 struct ext2_inode *get_root_inode(struct ext2_inode  *inode_table);
