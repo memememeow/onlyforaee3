7f03b32119b47f4067afb11eb76fa4039aae3dcd
Yuhan Shao <yuhan.shao@mail.utoronto.ca>
2018-08-02 19:52:21 -0400

    ls

--- ext2_ls.c
+++ ext2_ls.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
 #include <errno.h>
 #include "ext2.h"
 
