a46f9d056d54cd960b657cf77469e7c8ba458f73
wang huan <wanghuan@do-you-know-BTS.local>
2018-08-02 17:56:59 -0400

    i_mode

--- ext2_mkdir.c
+++ ext2_mkdir.c
@@ -26,7 +26,7 @@ int main (int argc, char **argv) {
     }
 
     unsigned char *disk = get_disk_loc(argv[1]);
-    // struct ext2_group_desc *gd = get_group_descriptor_loc(disk);
+    struct ext2_group_desc *gd = get_group_descriptor_loc(disk);
     struct ext2_super_block *sb = get_superblock_loc(disk);
     struct ext2_inode *i_table = get_inode_table_loc(disk);
     unsigned char *i_bitmap = get_inode_bitmap_loc(disk);
@@ -63,25 +63,27 @@ int main (int argc, char **argv) {
     struct ext2_inode *tar_inode = &(i_table[i_num - 1]);
 
     // Init the inode
-    tar_inode->i_mode = EXT2_S_IFREG;
+    tar_inode->i_mode = EXT2_S_IFDIR;
     tar_inode->i_size = EXT2_BLOCK_SIZE;
     tar_inode->i_links_count = 0;
     tar_inode->i_blocks = 0;
-
-    if (add_new_entry(disk, tar_inode, (unsigned int)i_num, get_file_name(argv[2]), 'd') == -1) {
-        printf("ext2_mkdir: Fail to add new directory entry in directory: %s\n", argv[3]);
-        exit(0);
-    }
-
-    if (add_new_entry(disk, tar_inode, (unsigned int)get_inode_num(disk, parent_inode), get_file_name(parent_path), 'd') == -1) {
-        printf("ext2_mkdir: Fail to add new directory entry in directory: %s\n", argv[3]);
-        exit(0);
-    }
 
     // Create a new entry in directory
     if (add_new_entry(disk, parent_inode, (unsigned int)i_num, get_file_name(argv[2]), 'd') == -1) {
         printf("ext2_mkdir: Fail to add new directory entry in directory: %s\n", argv[3]);
         exit(0);
     }
+
+    if (add_new_entry(disk, tar_inode, (unsigned int)i_num, ".", 'd') == -1) {
+        printf("ext2_mkdir: Fail to add new directory entry in directory: %s\n", argv[3]);
+        exit(0);
+    }
+
+    if (add_new_entry(disk, tar_inode, (unsigned int)get_inode_num(disk, parent_inode), "..", 'd') == -1) {
+        printf("ext2_mkdir: Fail to add new directory entry in directory: %s\n", argv[3]);
+        exit(0);
+    }
+
+    gd->bg_used_dirs_count ++;
     return 0;
 }
